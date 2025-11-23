83b9c216f15d3eda4a485cc93fea633e3dce3f02
cracknum <79014366+cracknum@users.noreply.github.com>
2025-11-23 18:26:42 +0800

    将viewwindow的颜色置为黑色

--- App/main.cpp
+++ App/main.cpp
@@ -15,22 +15,22 @@ int main(int argc, char** argv)
   int windowHeight = 1080;
   ViewWindowConfig config1;
   config1.m_ViewPort = ViewWindowConfig::ViewPort(0, 0, 0.5, 0.5);
-  config1.m_BackgroundColor = ViewWindowConfig::Color(1.0f, .0f, .0f, 1.0f);
+  config1.m_BackgroundColor = ViewWindowConfig::Color(0.0f, .0f, .0f, 1.0f);
 
   ViewWindowConfig config2;
   config2.m_ViewPort =
     ViewWindowConfig::ViewPort(0.5, 0, 0.5, 0.5);
-  config2.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 1.0f, .0f, 1.0f);
+  config2.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 0.0f, .0f, 1.0f);
 
   ViewWindowConfig config3;
   config3.m_ViewPort =
     ViewWindowConfig::ViewPort(0, 0.5, 0.5, 0.5);
-  config3.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 1.0f, .0f, 1.0f);
+  config3.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 0.0f, .0f, 1.0f);
 
   ViewWindowConfig config4;
   config4.m_ViewPort = ViewWindowConfig::ViewPort(
     0.5, 0.5, 0.5, 0.5);
-  config4.m_BackgroundColor = ViewWindowConfig::Color(1.0f, 0.0f, .0f, 1.0f);
+  config4.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 0.0f, .0f, 1.0f);
 
   window->addViewWindow(config1);
   window->addViewWindow(config2);
