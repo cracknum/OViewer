44317cddc0d1185fcd336506944d0f6eecb2884f
cracknum <79014366+cracknum@users.noreply.github.com>
2025-11-26 22:26:05 +0800

    改为使用vtk做显示框架

--- UI/src/MainWindow.cpp
+++ UI/src/MainWindow.cpp
@@ -2,18 +2,13 @@
 #include "DicomReader.hpp"
 #include "ImageInformation.hpp"
 #include "ProjectManagePanel.h"
-#include "Quad.h"
-#include "QuadConfig.h"
-#include "ViewWindowBase.h"
-#include "ViewWindowConfig.h"
-#include "Window.h"
 #include <QFileDialog>
 #include <QGridLayout>
+#include <QVTKOpenGLNativeWidget.h>
 #include <SARibbonBar.h>
 #include <SARibbonCategory.h>
 #include <itkImage.h>
 #include <spdlog/spdlog.h>
-
 
 namespace
 {
@@ -23,7 +18,7 @@ struct MainWindow::Impl
 {
   FloatDicomReader::SeriesVector m_SeriesVector;
   ProjectManagePanel* m_Panel;
-  Window* m_Window;
+  QVTKOpenGLNativeWidget* m_Window;
   Impl()
     : m_Panel(nullptr)
     , m_Window(nullptr)
@@ -65,37 +60,11 @@ void MainWindow::initHeader()
 
 void MainWindow::initCentral()
 {
-  auto* window = new Window();
-  ViewWindowConfig config1;
-  config1.m_ViewPort = ViewWindowConfig::ViewPort(0, 0, 0.5, 0.5);
-  config1.m_BackgroundColor = ViewWindowConfig::Color(1.0f, .0f, .0f, 1.0f);
-  config1.m_Id = 0;
-
-  ViewWindowConfig config2;
-  config2.m_ViewPort = ViewWindowConfig::ViewPort(0.5, 0, 0.5, 0.5);
-  config2.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 0.0f, 1.0f, 1.0f);
-  config2.m_Id = 1;
-
-  ViewWindowConfig config3;
-  config3.m_ViewPort = ViewWindowConfig::ViewPort(0, 0.5, 0.5, 0.5);
-  config3.m_BackgroundColor = ViewWindowConfig::Color(1.0f, 0.0f, .0f, 1.0f);
-  config3.m_Id = 2;
-
-  ViewWindowConfig config4;
-  config4.m_ViewPort = ViewWindowConfig::ViewPort(0.5, 0.5, 0.5, 0.5);
-  config4.m_BackgroundColor = ViewWindowConfig::Color(0.0f, 0.0f, 1.0f, 1.0f);
-  config4.m_Id = 4;
-
-  window->addViewWindow(config1);
-  window->addViewWindow(config2);
-  window->addViewWindow(config3);
-  window->addViewWindow(config4);
-  m_Impl->m_Window = window;
-
+  m_Impl->m_Window = new QVTKOpenGLNativeWidget();
   auto* centerWidget = new QWidget;
   auto* layout = new QGridLayout;
   m_Impl->m_Panel = new ProjectManagePanel;
-  layout->addWidget(window, 0, 0, 1, 2);
+  layout->addWidget(m_Impl->m_Window, 0, 0, 1, 2);
   layout->addWidget(m_Impl->m_Panel, 0, 2, 1, 1);
 
   layout->setColumnStretch(0, 1);
