22b9c8167f13b547a88252daad5c1dbf0a6b03e8
zhaolei <79014366+cracknum@users.noreply.github.com>
2026-01-14 17:25:21 +0800

    添加仿真模块

---
+++ Simulation/SpringMass/include/SpringMassExport.h
@@ -0,0 +1,13 @@
+/*--------------------------------------------------------------
+ * do not modify this file, which is generate by cmake
+ *-------------------------------------------------------------*/
+#ifndef SPRINGMASS_EXPORT_OPENGL_H
+#define SPRINGMASS_EXPORT_OPENGL_H
+
+#ifdef SPRINGMASS_EXPORTS
+    #define SPRINGMASS_API __declspec(dllexport)
+#else
+    #define SPRINGMASS_API __declspec(dllimport)
+#endif
+
+#endif //SPRINGMASS_EXPORT_OPENGL_H
